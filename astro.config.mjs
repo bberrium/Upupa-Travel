4ffefe4de2869cca7aa6c91f4008017389f6f3c6
Anahit <blueberrytea1111@gmail.com>
2026-07-22 01:12:00 +0400

    Add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bberrium.github.io',
+  base: '/Upupa-Travel',
   vite: {
     plugins: [tailwindcss()]
   }
