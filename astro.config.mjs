d95d524d4403c8272df477681efdf1cfdbfd665f
Anahit <blueberrytea1111@gmail.com>
2026-08-19 18:35:18 +0400

    fix tailwind css import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://bberrium.github.io',
   base: '/Upupa-Travel/',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()],
 });
